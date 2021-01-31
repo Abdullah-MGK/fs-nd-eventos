2e9512b88ad507f95e408f10eb7dabd2e4cb36aa
Abdullah-MGK <abdullah_mgk@outlook.com>
2021-01-31 14:02:11 +0300

    restructre

--- models.py
+++ models.py
@@ -3,23 +3,28 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 #from sqlalchemy import exc
 import json
-
-'''
-from decouple import config
-'''
+#from decouple import config
 
 database_name = "eventos"
-#database_path = "postgresql://{}/{}".format('localhost:5432', database_name)
-database_path = os.environ['DATABASE_URL']
+
+#NOTE: For Local Setup
+database_domain = "localhost:5432"
+database_path = "postgresql://{}/{}".format(database_domain, database_name)
+
+#NOTE: For Heroku Setup
+#database_path = os.environ['DATABASE_URL'] 
+
 db = SQLAlchemy()
 
 # binds a flask application and a SQLAlchemy service
-def setup_db(app):
+def setup_db(app, database_path=database_path):
   app.config["SQLALCHEMY_DATABASE_URI"] = database_path
   app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
   db.app = app
   db.init_app(app)
-  #db.create_all()
+
+  #NOTE: comment this for Heroku Setup
+  db.create_all()
 
 def db_drop_and_create_all():
   db.drop_all()
